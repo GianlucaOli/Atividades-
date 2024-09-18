d092f13b4df234291ffb980e9f656e5c99c43984
Gianluca <gianlucamarcia123@gmail.com>
2024-09-18 16:56:10 -0300

    Delete Aula4.2 directory

--- Aula 4.2/1.Fatorial.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-let numero = Number(prompt("Digite seu número: "));
-
-let resultado = 1;
-for (let i = 1; i <= numero; i++) {
-  resultado *= i;
-}
-console.log(`O fatorial é ${resultado}`);
--- Aula 4.2/3.Tabuada.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync") ()
-const numero = Number(prompt("digite seu número: "))
-if (!isNaN(numero)) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${numero} x ${i} = ${numero * i}`);
-    }
-} else {
-    console.log("Por favor, digite um número válido.");
-}
